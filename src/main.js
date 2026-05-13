a450c5dda95ebf6038ee7cff9a4aa8f48cbdfa46
psulca <psulcasanchez@gmail.com>
2026-05-13 17:19:48 -0500

    feat: register pinia, router and i18n in main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import pinia from './pinia.js';
+import router from './router.js';
+import i18n from './i18n.js';
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(pinia)
+    .use(router)
+    .use(i18n)
+    .mount('#app');
